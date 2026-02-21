dc392e3bd24fea4506a8d61bf218c6fb5ca82505
Nidhi <your.email@gmail.com>
2026-02-21 11:12:30 +0530

    exploring routes

--- src/app.js
+++ src/app.js
@@ -3,16 +3,27 @@ const express = require('express');
 //create instance of express application
 const app = express();
 
-
-//request handlers to send response to client request
+app.get('/users',(req,res)=>{
+    res.send({firstname: "Nidhi", lastname:"Pal"});
+})
+app.post('/users',(req,res)=>{
+    console.log("Data save to DB");
+    res.send("Data savig to database");
+})
+app.put('/users',(req,res)=>{
+    console.log("Data updated in DB");
+    res.send("Data updated in database");
+})
+app.patch('/users',(req,res)=>{
+    console.log("data patched in db");
+    res.send("data patched in database");
+})
+app.delete('/users',(req,res)=>{
+    console.log("data deleted from db");
+    res.send("data deleted from database");
+})
 app.use("/test",(req,res)=>{
     res.send("Hello sending response to test request");
-});
-app.use("/hello",(req,res)=>{
-    res.send("Hello sending response to hello request");
-});
-app.use("/",(req,res)=>{
-    res.send("Hello sending response to request");
 });
 
 
@@ -20,4 +31,9 @@ app.use("/",(req,res)=>{
 app.listen(7777, ()=>{
     console.log("server is listening successfully on port 7777");
 });
+
+/**
+ * order of routes matter
+ * it maches with start 
+ */
 
